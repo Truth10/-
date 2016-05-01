877d1364bd3146f0f15d3ff93e18fa6c937dfbb5
mac <mac@WangJianWendeMacBook.local>
2016-05-01 11:40:03 +0800

    实现登录VC的全部功能;包括自动登录,存取用户数据,显示登录动画提示
    1.点击登录按钮,判断用户名和密码是否正确,正确则跳转到listVC;错误则给出提示
    2.用户数据的存取,使用存储在偏好设置文件夹下;当用户登录成功时存储;当控制器的view加载成功,且控件都创建好并且约束也添加好时再加载.
    3.使用第三方框架MBProgressHud来显示提示的动画

--- 微信电话本(纯代码版)/HMLoginController.m
+++ 微信电话本(纯代码版)/HMLoginController.m
@@ -7,7 +7,11 @@
 //
 
 #import "HMLoginController.h"
-
+#import "HMListController.h"
+#define HM_USERNAME @"HM_USERNAME"//用户名
+#define HM_PASSWORD @"HM_PASSWORD"//密码
+#define HM_REMSWITCH @"HM_REMSWITCH"//密码
+#define HM_AUTOSWITCH @"HM_AUTOSWITCH"//密码
 @interface HMLoginController ()
 
 @property (nonatomic,weak) UITextField *nameField;//用户名
@@ -21,16 +25,142 @@
 @end
 
 @implementation HMLoginController
-
+#pragma mark - 当控制器的view加载完成时来调用此方法
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     //设置界面
     [self setUpUI];
     
     //添加约束
     [self setUpConstraints];
+    
+#pragma mark - 读取用户登录信息
+    //1.创建偏好设置对象
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    
+    //2.读取数据
+    self.nameField.text = [defaults objectForKey:HM_USERNAME];
+    self.passwordField.text = [defaults objectForKey:HM_PASSWORD];
+    self.remSwitch.on = [defaults boolForKey:HM_REMSWITCH];
+    self.autoLoginSwitch.on = [defaults boolForKey:HM_AUTOSWITCH];
+    HMLog(@"%@",self.nameField.text);
+    //3.执行相应的操作
+    //如果记住密码开关关闭
+    if (!self.remSwitch.isOn) {
+        //将密码框文字设为nil
+        self.passwordField.text = nil;
+    }
+    //如果自动登录开关打开,就点击登录按钮
+    if (self.autoLoginSwitch.isOn) {
+        [self loginBtnClick:self.loginBtn];
+    }
+    
+    
+    
+    //MARK: - 监听文本框的编辑事件
+    [self.nameField addTarget:self action:@selector(textChange:) forControlEvents:UIControlEventEditingChanged];
+    [self.passwordField addTarget:self action:@selector(textChange:) forControlEvents:UIControlEventEditingChanged];
+    
+    //MARK: - 监听开关的值改变事件
+    [self.remSwitch addTarget:self action:@selector(remSwitchValueChange:) forControlEvents:UIControlEventValueChanged];
+    [self.autoLoginSwitch addTarget:self action:@selector(autoLoginSwitchValueChange:) forControlEvents:UIControlEventValueChanged];
+    
+    //MARK: - 监听按钮的点击事件
+    [self.loginBtn addTarget:self action:@selector(loginBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //调用文本框的编辑事件,判断文本框内容是否为空
+    [self textChange:nil];
 }
+
+#pragma mark - 文本框的编辑事件
+- (void)textChange:(UITextField *)sender{
+    //MARK: - 获取文本框数据
+    NSString *name = self.nameField.text;
+    NSString *password = self.passwordField.text;
+    
+    HMLog(@"%@",sender.text);
+    //当两个文本框内内容都不为空时登录按钮才能点击
+    if (name.length > 0 && password.length > 0) {
+        self.loginBtn.enabled = YES;
+    }else{
+        self.loginBtn.enabled = NO;
+    }
+}
+
+#pragma mark - 开关的值改变事件,开关的联动
+//记录密码开关
+- (void)remSwitchValueChange:(UISwitch *)sender{
+    //如果记录密码开关关闭,则自动登录开关也必须关闭
+    if (!self.remSwitch.isOn) {
+        self.autoLoginSwitch.on = NO;
+        
+    }
+
+}
+
+//自动登录开关
+- (void)autoLoginSwitchValueChange:(UISwitch *)sender{
+    //如果自动登录开关打开,则记录密码开关也必须打开
+    if (self.autoLoginSwitch.isOn) {
+        self.remSwitch.on = YES;
+        
+    }
+    
+}
+#pragma mark - 按钮的点击事件
+- (void)loginBtnClick:(UIButton *)sender{
+    HMLog(@"登录按钮被点击了");
+    //1.获取用户名和密码
+    NSString *name = self.nameField.text;
+    NSString *password = self.passwordField.text;
+    
+#pragma mark - 显示登录动画
+    MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:self.view];
+    [self.view addSubview:hud];//添加到父控件
+    hud.mode = MBProgressHUDAnimationFade;//只显示文本
+    hud.labelText = @"正在登录中...";//显示的内容
+    [hud show:YES];
+    
+    //延迟2秒执行下边的代码
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        //2.如果用户名和密码输入正确,则登录成功,并跳转到列表VC
+        if ([name isEqualToString:@"wang"] && [password isEqualToString:@"0510"]) {
+            //1.隐藏登录动画
+            [hud hide:NO];
+            [hud removeFromSuperview];//从父控件移除
+            
+            //2.创建列表控制器
+            HMListController *listVc = [[HMListController alloc] init];
+
+            //3.跳转到列表控制器
+            [self.navigationController pushViewController:listVc animated:YES];
+            
+            //4.保存用户登录信息
+            //4.1创建偏好设置对象
+            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+            
+            //4.2保存数据
+            [defaults setObject:name forKey:HM_USERNAME];
+            [defaults setObject:password forKey:HM_PASSWORD];
+            [defaults setBool:self.remSwitch.isOn forKey:HM_REMSWITCH];
+            [defaults setBool:self.autoLoginSwitch.isOn forKey:HM_AUTOSWITCH];
+
+            //4.3同步
+            [defaults synchronize];
+            HMLog(@"%@",NSHomeDirectory());
+        }else {
+            
+            //仅显示文字
+            hud.mode = MBProgressHUDModeText;
+            hud.labelText = @"您输入的用户名或密码错误";
+            //显示1秒后隐藏
+            [hud hide:YES afterDelay:1.0];
+        
+        }
+        
+    });
+}
+
 #pragma mark - 设置界面
 - (void)setUpUI{
     self.navigationItem.title = @"欢迎使用微信电话本";//标题
@@ -46,6 +176,7 @@
     UITextField *passwordField = [[UITextField alloc] init];
     passwordField.placeholder = @"请输入密码";
     passwordField.clearButtonMode = UITextFieldViewModeWhileEditing;//右侧的清除按钮
+    passwordField.secureTextEntry = YES;//密文显示
     [self.view addSubview:passwordField];
     self.passwordField = passwordField;
     
@@ -74,11 +205,12 @@
     //登录按钮
     UIButton *loginBtn = [[UIButton alloc] init];
     [loginBtn setTitle:@"登  录" forState:UIControlStateNormal];
-    loginBtn.backgroundColor = [UIColor blueColor];
-    //loginBtn.
+    [loginBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];//给正常状态下文字设置背景色
+    [loginBtn setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];//给不可用状态下文字设置背景色
+    
     [self.view addSubview:loginBtn];
     self.loginBtn = loginBtn;
-   // loginBtn.enabled = NO;//默认按钮不能点击
+    loginBtn.enabled = NO;//默认按钮不能点击
     
     
 }
@@ -126,7 +258,7 @@
     [self.loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.passwordField.mas_bottom).offset(90);//顶部等于密码文本框底部+90
         make.left.right.equalTo(self.nameField);//左右两侧与用户名文本框对齐
-    
+        
     }];
     
 }
